31b17b127bdeb448c15b09f168984112ced995f0
andy <c1491124@gmail.com>
2022-12-29 16:32:08 +0800

    day05 completed

--- src/Day05.kt
+++ src/Day05.kt
@@ -1,8 +1,43 @@
 fun main() {
     val input = readInput("Day05")
-    val lists = List(9) { mutableListOf<Char>() }
-    val moves = mutableListOf<String>()
-    // init lists and moves
+
+    fun part1(input: List<String>) {
+        val lists = MutableList(9) { mutableListOf<Char>() }
+        val moves = mutableListOf<String>()
+        // init lists and moves by parsing input
+        val parsedMoves = parseInputAndInit(input, lists, moves)
+        parsedMoves.forEach {
+            val (num, from, to) = it
+            val toRemove = lists[from].subList(0, num)
+            lists[to].reverse()
+            lists[to] += toRemove
+            lists[to].reverse()
+            lists[from] = lists[from].drop(num).toMutableList()
+        }
+        lists.forEach { print(it[0]) }
+    }
+
+    fun part2(input: List<String>) {
+        val myLists2 = MutableList(9) { mutableListOf<Char>() }
+        val moves = mutableListOf<String>()
+        // init lists and moves by parsing input
+        val parsedMoves = parseInputAndInit(input, myLists2, moves)
+        parsedMoves.forEach {
+            val (num, from, to) = it
+            val toRemove = myLists2[from].subList(0, num)
+            myLists2[to] = (toRemove + myLists2[to]).toMutableList()
+            myLists2[from] = myLists2[from].drop(num).toMutableList()
+        }
+        myLists2.forEach { print(it[0]) }
+    }
+    part1(input)
+    println()
+    part2(input)
+}
+// this function parse input and init the list of crates and move commands,it take the empty lists in,
+// then init(mutate) them , the returned moves is something like [[2,7,2],[1,4,8],.....]
+fun parseInputAndInit(input: List<String>, lists: List<MutableList<Char>>, moves: MutableList<String>):
+        List<List<Int>> {
     input.forEachIndexed { index, s ->
         if (index <= 9) {
             s.forEachIndexed { i, c ->
@@ -14,36 +49,8 @@ fun main() {
             moves += s
         }
     }
-    val parsedMoves = moves.map {
+    return moves.map {
         val components = it.split(" ")
         listOf(components[1].toInt(), components[3].toInt() - 1, components[5].toInt() - 1)
     }
-
-    // resolve command
-    fun part1(lists: List<MutableList<Char>>) {
-        val myLists = lists.map { it }.toMutableList()  // use map() as deep copy
-        parsedMoves.forEach {
-            val (num, from, to) = it
-            val toRemove = myLists[from].subList(0, num)
-            myLists[to].reverse()
-            myLists[to] += toRemove
-            myLists[to].reverse()
-            myLists[from] = myLists[from].drop(num).toMutableList()
-        }
-        myLists.forEach { print(it[0]) }
-    }
-
-    fun part2(lists: List<MutableList<Char>>) {
-        val myLists2 = lists.map { it }.toMutableList()   // use copyOf() as deep copy
-        parsedMoves.forEach {
-            val (num, from, to) = it
-            val toRemove = myLists2[from].subList(0, num)
-            myLists2[to] = (toRemove + myLists2[to]).toMutableList()
-            myLists2[from] = myLists2[from].drop(num).toMutableList()
-        }
-        myLists2.forEach { print(it[0]) }
-    }
-    part1(lists)
-    println()
-    part2(lists)
 }
